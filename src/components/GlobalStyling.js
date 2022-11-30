05f3bea8a07c176b91a1905b5dd13611c50402c0
Cecilia Råden <you@example.com>
2022-11-30 20:55:33 +0100

    added styling and a picture and a button

--- src/components/GlobalStyling.js
+++ src/components/GlobalStyling.js
@@ -5,3 +5,25 @@ export const Wrapper = styled.div`
 display: flex;
 justify-content: center;
 `
+export const Text = styled.p`
+font-family: 'Great Vibes', cursive;
+font-size: 40px;
+color: white;
+`
+
+export const Container = styled.div`
+width: 80%;
+height: auto;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+text-align: center;
+margin: 15px;
+position: absolute;
+top: 10%;
+
+@media (min-width: 1025px) {
+  width: 30%;
+  }
+  `
